aec02c736e0c46fe8395387a0b285adf5f1e9de3
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-12-25 21:49:16 +1000

    Add a basic editor with markdown renderer

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -9,14 +9,15 @@
     <div class="columns">
       <div class="column one-half">
         <textarea 
+          id="markdown"
           class="textarea"
           v-model="content"
         />
       </div>
       <div class="column one-half">
-        <textarea 
-          class="textarea"
-          v-model="content" 
+        <div 
+          id="rendered-markdown"
+          v-html="html"
         />
       </div>
     </div>
@@ -24,7 +25,10 @@
 </template>
 
 <script lang="ts">
-import { createComponent, ref } from '@vue/composition-api'
+import { createComponent, ref, watch } from '@vue/composition-api'
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github.css';
 
 import ValidatorInput from '@/components/ValidatorInput.vue'
 import { minLength } from '@/components/validation'
@@ -32,20 +36,72 @@ import { minLength } from '@/components/validation'
 export default createComponent({
   name: 'NewPost',
 
+  props: {},
+
   components: {
     ValidatorInput,
   },
 
+
   setup() {
+    const renderer = new marked.Renderer();
+    renderer.heading = (text, level) => {
+      return `
+        <h${level} class="markdown-h${level}">
+          ${text}
+        </h${level}>`
+    }
+
+    const options: marked.MarkedOptions = {
+      gfm: true,
+      breaks: true,
+      highlight: (code: string) => {
+        return hljs.highlightAuto(code).value
+      },
+      renderer
+    }
+
     const title = ref('')
     const content = ref('')
+    const html = ref('')
     const titleValidation = [ minLength({ min: 5, max: 100 }) ]
+
+    watch(() => content.value, (val) => {
+      marked(content.value, options, (err, res) => {
+        if (err) {
+          return
+        }
+        html.value = res
+      })
+    })
 
     return {
       content,
+      html,
       title,
       titleValidation,
     }
   }
 })
 </script>
+
+<style>
+#markdown, #rendered-markdown {
+  min-height: 400px;
+}
+
+.markdown-h1 { font-size: 1.6rem !important; }
+.markdown-h2 { font-size: 1.5rem !important; }
+.markdown-h3 { font-size: 1.4rem !important; }
+.markdown-h4 { font-size: 1.3rem !important; }
+.markdown-h5 { font-size: 1.2rem !important; }
+.markdown-h6 { font-size: 1.1rem !important; }
+
+#rendered-markdown {
+  padding: calc(0.75em - 1px);
+  border: 1px solid;
+  border-color: #dbdbdb;
+  border-radius: 4px;
+  color: #363636;
+}
+</style>
