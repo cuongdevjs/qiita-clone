519883bae179dd622c04e28a39ae94125eb10047
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-12-27 17:18:00 +1000

    remove unused files

--- src/store/users/types.ts
+++
@@ -1,4 +0,0 @@
-import { Store } from 'vuex'
-import { Mutations, Module, createMapper, Actions, Getters } from 'vuex-smart-module'
-import random from 'lodash/random'
-import { axios } from '@/resources/mockAxios'
